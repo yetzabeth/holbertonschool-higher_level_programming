2e1667fb1fe64b00431d506d4cc9e99cfc4947be
yetzabeth <yetzaher@gmail.com>
2023-09-23 02:42:44 -0700

    tansk 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def islower(c):
+    i = ord(c)
+    if i > 96 and i < 123:
+        return True
+    else:
+        return False
+
+    
+def uppercase(str):
+    upper = ''
+    for i in str:
+        if islower(i):
+            i = chr(ord(i) - 32)
+        print('{}'.format(i), end='')
+    print('')
