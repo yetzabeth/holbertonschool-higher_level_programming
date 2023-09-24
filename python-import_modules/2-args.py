0da9df80f30032c94ed554fe0a3d1b8b84c0e0db
yetzabeth <yetzaher@gmail.com>
2023-09-24 08:57:03 -0700

    tansk 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    number = len(sys.argv)
+
+    if number == 1:
+        print("{} arguments.".format(number - 1))
+    elif number == 2:
+        print("{} arguments.".format(number - 1))
+    else:
+        print("{} arguments.".format(number - 1))
+
+    for i in range(1, number):
+        print("{}: {}".format(i, sys.argv[i]))
