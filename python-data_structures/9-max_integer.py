b53dd5605ae61b16883caaf4a9aebab684d05bee
yetzabeth <yetzaher@gmail.com>
2023-09-28 02:46:11 -0700

    tansk 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        my_list.sort()
+        my_list.reverse()
+        return my_list[0]
