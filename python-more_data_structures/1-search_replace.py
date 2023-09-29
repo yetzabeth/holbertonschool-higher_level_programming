24d48901bcf893fe8a0074f24c0974122ad0f92e
yetzabeth <yetzaher@gmail.com>
2023-09-29 03:39:42 -0700

    tansk 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     new = my_list.copy()
     for i in range(len(new)):
         if new[i] == search:
-            new[i] == replace
+            new[i] = replace
     return new
