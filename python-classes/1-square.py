ae0db6048ee4fa9108253aaf48a2a5764c5a3892
yetzabeth <yetzaher@gmail.com>
2023-09-30 13:59:08 -0700

    tansk 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''This module write a class that define a square'''
+
+
+class Square:
+    '''this class define a square whith its  size'''
+    def __init__(self, size):
+        self.__size = size
