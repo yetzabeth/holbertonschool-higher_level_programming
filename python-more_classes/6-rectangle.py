d9b8b6b1d1461c36d0122800ce9fa283a409fc44
yetzabeth <yetzaher@gmail.com>
2023-10-05 19:20:50 +0000

    tansk 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -8,9 +8,12 @@ class Rectangle:
     """
     init
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     def area(self):
         return self.width * self.height
