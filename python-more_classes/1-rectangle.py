fedca1669d837c618019e4222a57c9201284d507
yetzabeth <yetzaher@gmail.com>
2023-10-05 15:06:13 +0000

    tansk 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Write a class Rectangle that defines a rectangle'''
+
+
+class Rectangle:
+    '''Defines new instance of the class'''
+    def __init__(self, width=0, height=0)
