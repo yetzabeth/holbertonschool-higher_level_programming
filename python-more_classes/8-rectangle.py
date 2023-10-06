a474dc1eae00dc01f5b0a7622b70360ab943b352
yetzabeth <yetzaher@gmail.com>
2023-10-06 01:48:29 +0000

    tansk 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     init
     """
     number_of_instances = 0
-    
+
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
@@ -63,6 +63,15 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError('rect_2 must be an instance of Rectangle')
+        if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
+            return rect_1
 
     @property
     def height(self):
