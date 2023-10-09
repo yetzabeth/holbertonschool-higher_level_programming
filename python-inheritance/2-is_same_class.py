ba39268f9fd8ef8ec6bb88a05020f6fe7778c77c
yetzabeth <yetzaher@gmail.com>
2023-10-09 02:40:38 +0000

    tansk 1

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' Exact same object  '''
+
+
+def is_same_class(obj, a_class):
+    ''' function  '''
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
