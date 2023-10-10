e51b1b3f27c292aca0076f3c9fa49aa264dba12c
yetzabeth <yetzaher@gmail.com>
2023-10-10 19:19:52 +0000

    tansk 6

---
+++ python-inheritance/__pycache__/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Class that define a BaseGeometry object"""
+    pass
+
+    def area(self):
+        """Calculates the area"""
+        raise Exception("area() is not implemented")
