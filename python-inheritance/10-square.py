3f127f363ea5eb680ef801d116940658a8b8698c
yetzabeth <yetzaher@gmail.com>
2023-10-20 16:52:41 +0000

    tansk 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class based on Rectangle baseclass"""
+
+    def __init__(self, size):
+        """To create a new instance"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of a square"""
+        return self.__size * self.__size
