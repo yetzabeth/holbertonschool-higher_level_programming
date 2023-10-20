a7d14756c2a089841e39d603385d791fac41a4d8
yetzabeth <yetzaher@gmail.com>
2023-10-20 19:00:02 +0000

    tansk 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Write a function that appends a string at the end of a text file (UTF8)
+and returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """Returns the number of characters added"""
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
