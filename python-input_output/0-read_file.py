5e30d1443f41d8e793348201e0ac20f9d15dad94
yetzabeth <yetzaher@gmail.com>
2023-10-20 18:24:48 +0000

    tansk 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Write a function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """Read the test file"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
