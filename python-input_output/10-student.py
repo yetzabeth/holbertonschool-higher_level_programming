15ffdd6b0add7c1ba7eab7100bec3b08ef16c18e
yetzabeth <yetzaher@gmail.com>
2023-10-23 03:04:04 +0000

    tansk 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -2,3 +2,24 @@
 """
 Write a class Student that defines a student
 """
+
+
+class Student():
+    """Class Student with attributes"""
+    def __init__(self, first_name, last_name, age):
+        """To create a new instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if type(attrs) == list:
+            new_list == {}
+            for item in attrs:
+                for key, value in self.__dict__.item():
+                    if key == item:
+                        new_list[item] = value
+            return new_list
+
+        return self.__dict__
