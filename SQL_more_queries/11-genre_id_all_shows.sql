eba55de249fcd70af75d17534b9b85bdcbaa3ef1
yetzabeth <yetzaher@gmail.com>
2023-11-15 16:01:24 +0000

    tansk 11 and 12

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,3 @@
--- Write a script that lists all shows contained in the database hbtn_0d_tvshows
+-- Import the database dump of hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id=tv_show_genres.shows.id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ON tv_shows.id=tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
